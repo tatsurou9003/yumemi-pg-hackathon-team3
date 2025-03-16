3542c2f22acac9f7032204e7b40a55093c1fda81
ユーザー名 <luck@imai.am>
2025-03-16 17:00:18 +0900

    グループリスト作成

---
+++ client/src/components/common/group-list/group-list.tsx
@@ -0,0 +1,27 @@
+import { Group, GroupData } from "@/types/groupData";
+import { Avatar, AvatarFallback, AvatarImage } from "../avatar/avatar";
+import { Link } from "@tanstack/react-router";
+
+const GroupList = ({ groupData }: GroupData) => {
+  return (
+    <div className="flex flex-col bg-[#FFEADD] w-full">
+      {groupData.map((group: Group) => (
+        <Link
+          //to={`/home/${group.groupId}`}
+          to="/about" //暫定的に
+          className="float-left p-[13px] flex items-center gap-[21px] font-[Inter]"
+        >
+          <Avatar>
+            <AvatarImage src={group.groupImage} />
+            <AvatarFallback className="bg-white">
+              {group.groupName.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+          {group.groupName}({group.count})
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default GroupList;
