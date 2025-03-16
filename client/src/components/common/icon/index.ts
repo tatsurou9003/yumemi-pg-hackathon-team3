3e6da10e57b239707c37f8f77c4d1bd162a0f78c
ユーザー名 <luck@imai.am>
2025-03-16 19:29:46 +0900

    svgコンポーネント作成

--- client/src/components/common/icon/index.ts
+++ client/src/components/common/icon/index.ts
@@ -1,2 +1,4 @@
+export { default as Character } from "./character";
 export { default as ChevronLeft } from "./chevron-left";
 export { default as HamburgerButton } from "./hamburger-button";
+export { default as Plus } from "./plus";
