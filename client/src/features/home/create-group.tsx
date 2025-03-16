a1e926b17456e30c24f58fda4a6c9ccd66f9eec3
ユーザー名 <luck@imai.am>
2025-03-16 19:30:53 +0900

    グループ作成ボタンの追加

---
+++ client/src/features/home/create-group.tsx
@@ -0,0 +1,21 @@
+import { Plus } from "@/components/common/icon";
+import { useNavigate } from "@tanstack/react-router";
+
+const CreateGroup = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      onClick={() => {
+        //navigate({ to: "/home/group" });
+        navigate({ to: "/about" });
+      }}
+      className="bg-[#FFEADD] text-[#FF6200] font-[Roboto] font-medium flex gap-[6px] items-center rounded-[36px] p-[6px_36px_6px_6px] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.10)] hover:shadow-[0px_4px_8px_0px_rgba(0,0,0,0.15)] hover:-translate-y-0.5 transition-all duration-200"
+    >
+      <Plus />
+      グループを作成する
+    </button>
+  );
+};
+
+export default CreateGroup;
