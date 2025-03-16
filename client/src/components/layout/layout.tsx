b6a2b24ed27c0c7397a84bd305f23fdc6f16cb2d
Ren Yagi <r.yagi@4nonome.com>
2025-03-17 00:19:57 +0900

    Update client/src/components/layout/layout.tsx
    Co-authored-by: Imai Raku <71510837+luck-tech@users.noreply.github.com>

--- client/src/components/layout/layout.tsx
+++ client/src/components/layout/layout.tsx
@@ -52,8 +52,21 @@ export const Layout: React.FC<LayoutProps> = ({
         avatar={avatar}
         onSidebar={handleSidebar}
       />
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black/25 backdrop-blur-[0.5px] z-30 transition-all duration-300"
+          onClick={handleSidebar}
+        />
+      )}
+      <div
+        className={`fixed top-[56px] left-0 h-[calc(100vh_-_56px)] z-40 transition-transform duration-300 ease-out ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <Sidebar version={version} onLogout={onLogout} />
+      </div>
       <div className="flex flex-row">
-        {isSidebarOpen && <Sidebar version={version} onLogout={onLogout} />}
+        <main className="flex-1 bg-white">{children}</main>
         <main className="flex-1 bg-white">{children}</main>
       </div>
     </div>
