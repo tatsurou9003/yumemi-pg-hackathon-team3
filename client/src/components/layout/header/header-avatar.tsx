4dae320f00d92de7f77d6ab4116044111629b9ba
renyagi <r.yagi@4nonome.com>
2025-03-16 21:39:43 +0900

    挙動の修正

--- client/src/components/layout/header/header-avatar.tsx
+++ client/src/components/layout/header/header-avatar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import {
   Avatar,
   AvatarFallback,
@@ -12,31 +12,57 @@ interface AvatarProps {
 
 const HeaderAvatar = ({ src }: AvatarProps) => {
   const [isUserCardOpen, setIsUserCardOpen] = useState(false);
+  const userCardRef = useRef<HTMLDivElement | null>(null);
+  const avatarRef = useRef<HTMLButtonElement | null>(null);
+
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (
+      userCardRef.current &&
+      avatarRef.current &&
+      !userCardRef.current.contains(event.target as Node) &&
+      !avatarRef.current.contains(event.target as Node)
+    ) {
+      setIsUserCardOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   const toggleUserCard = () => {
     setIsUserCardOpen((prev) => !prev);
   };
 
   return (
     <div className="relative inline-block">
-      <button onClick={toggleUserCard} className="border-none bg-transparent">
+      <button
+        onClick={toggleUserCard}
+        ref={avatarRef}
+        className="border-none bg-transparent"
+      >
         <Avatar>
           <AvatarImage src={src} />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </button>
-      <div
-        className={`absolute top-[calc(100%_+_3px)] right-0 z-10 transition-opacity duration-200 ${
-          isUserCardOpen ? "opacity-100 visible" : "opacity-0 invisible"
-        }`}
-      >
-        <UserCard
-          name="John Doe"
-          src={src}
-          id="12345"
-          onSettings={() => console.log("Settings clicked")}
-          onCamera={() => console.log("Camera clicked")}
-        />
-      </div>
+      {isUserCardOpen && (
+        <div
+          ref={userCardRef}
+          className="absolute top-[calc(100%_+_3px)] right-0 z-10 transition-opacity duration-200"
+        >
+          <UserCard
+            name="John Doe"
+            src={src}
+            id="12345"
+            onSettings={() => console.log("Settings clicked")}
+            onCamera={() => console.log("Camera clicked")}
+          />
+        </div>
+      )}
     </div>
   );
 };
