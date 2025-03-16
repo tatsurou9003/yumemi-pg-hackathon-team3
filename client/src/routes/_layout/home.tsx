1c0ed97593d2830dd0837f09cb5dc77feb2f700e
ユーザー名 <luck@imai.am>
2025-03-16 19:31:32 +0900

    グループ作成ボタンの配置, ヘッダーイメージの配置

--- client/src/routes/_layout/home.tsx
+++ client/src/routes/_layout/home.tsx
@@ -1,5 +1,6 @@
 import GroupList from "@/components/common/group-list/group-list";
 import { createFileRoute } from "@tanstack/react-router";
+import CreateGroup from "@/features/home/create-group";
 
 export const Route = createFileRoute("/_layout/home")({
   component: RouteComponent,
@@ -24,13 +25,19 @@ const groupData = [
 function RouteComponent() {
   return (
     <div className="min-h-screen bg-[#FFBC92] text-xs">
-      <section className="font-[Inter] flex items-center justify-center bg-[#E8DDDD] flex-col p-[54px_0_14px_0] gap-[39px]">
-        <p>イベント開催中</p>
-        <button>イベント会場へ</button>
+      <section className="w-full">
+        <img
+          src="/src/assets/header.png"
+          alt="ヘッダー画像"
+          className="w-full"
+        />
       </section>
-      <section className="float-left w-full">
+      <section className="w-full text-[#743E3E]">
         <p className="p-[14px_0_9px_14px]">グループ一覧</p>
         <GroupList groupData={groupData} />
+      </section>
+      <section className="m-[20px_12px]">
+        <CreateGroup />
       </section>
     </div>
   );
