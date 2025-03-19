ced2b1d9f1c1e00412a0ecbf1c3cd5d6336d9b35
renyagi <r.yagi@4nonome.com>
2025-03-19 17:28:06 +0900

    アイコンの追加

---
+++ client/src/components/common/icon/send-brown.tsx
@@ -0,0 +1,27 @@
+import type { SVGProps } from "react";
+const SvgSendBrown = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <g clipPath="url(#send-brown_svg__a)">
+      <path
+        stroke="#743E3E"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.6}
+        d="M14.667 1.333 7.333 8.667m7.334-7.334L10 14.667l-2.667-6m7.334-7.334L1.333 6l6 2.667"
+      />
+    </g>
+    <defs>
+      <clipPath id="send-brown_svg__a">
+        <path fill="#fff" d="M0 0h16v16H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default SvgSendBrown;
