170e75c031a620fe17ebaec79530dc5740fb802c
ryu sugiyama <2005ryu427@gmail.com>
2025-03-22 22:46:25 +0900

    見た目が完成しました

--- client/src/routeTree.gen.ts
+++ client/src/routeTree.gen.ts
@@ -15,6 +15,7 @@ import { Route as LayoutImport } from "./routes/_layout";
 import { Route as IndexImport } from "./routes/index";
 import { Route as LayoutSigunpImport } from "./routes/_layout/sigunp";
 import { Route as LayoutProfileImport } from "./routes/_layout/profile";
+import { Route as LayoutMailImport } from "./routes/_layout/mail";
 import { Route as LayoutLoginImport } from "./routes/_layout/login";
 import { Route as LayoutHomeIndexImport } from "./routes/_layout/home/index";
 import { Route as LayoutHomeRoomIdImport } from "./routes/_layout/home/$roomId";
@@ -41,6 +42,12 @@ const LayoutSigunpRoute = LayoutSigunpImport.update({
 const LayoutProfileRoute = LayoutProfileImport.update({
   id: "/profile",
   path: "/profile",
+  getParentRoute: () => LayoutRoute,
+} as any);
+
+const LayoutMailRoute = LayoutMailImport.update({
+  id: "/mail",
+  path: "/mail",
   getParentRoute: () => LayoutRoute,
 } as any);
 
@@ -87,6 +94,13 @@ declare module "@tanstack/react-router" {
       preLoaderRoute: typeof LayoutLoginImport;
       parentRoute: typeof LayoutImport;
     };
+    "/_layout/mail": {
+      id: "/_layout/mail";
+      path: "/mail";
+      fullPath: "/mail";
+      preLoaderRoute: typeof LayoutMailImport;
+      parentRoute: typeof LayoutImport;
+    };
     "/_layout/profile": {
       id: "/_layout/profile";
       path: "/profile";
@@ -122,6 +136,7 @@ declare module "@tanstack/react-router" {
 
 interface LayoutRouteChildren {
   LayoutLoginRoute: typeof LayoutLoginRoute;
+  LayoutMailRoute: typeof LayoutMailRoute;
   LayoutProfileRoute: typeof LayoutProfileRoute;
   LayoutSigunpRoute: typeof LayoutSigunpRoute;
   LayoutHomeRoomIdRoute: typeof LayoutHomeRoomIdRoute;
@@ -130,6 +145,7 @@ interface LayoutRouteChildren {
 
 const LayoutRouteChildren: LayoutRouteChildren = {
   LayoutLoginRoute: LayoutLoginRoute,
+  LayoutMailRoute: LayoutMailRoute,
   LayoutProfileRoute: LayoutProfileRoute,
   LayoutSigunpRoute: LayoutSigunpRoute,
   LayoutHomeRoomIdRoute: LayoutHomeRoomIdRoute,
@@ -143,6 +159,7 @@ export interface FileRoutesByFullPath {
   "/": typeof IndexRoute;
   "": typeof LayoutRouteWithChildren;
   "/login": typeof LayoutLoginRoute;
+  "/mail": typeof LayoutMailRoute;
   "/profile": typeof LayoutProfileRoute;
   "/sigunp": typeof LayoutSigunpRoute;
   "/home/$roomId": typeof LayoutHomeRoomIdRoute;
@@ -153,6 +170,7 @@ export interface FileRoutesByTo {
   "/": typeof IndexRoute;
   "": typeof LayoutRouteWithChildren;
   "/login": typeof LayoutLoginRoute;
+  "/mail": typeof LayoutMailRoute;
   "/profile": typeof LayoutProfileRoute;
   "/sigunp": typeof LayoutSigunpRoute;
   "/home/$roomId": typeof LayoutHomeRoomIdRoute;
@@ -164,6 +182,7 @@ export interface FileRoutesById {
   "/": typeof IndexRoute;
   "/_layout": typeof LayoutRouteWithChildren;
   "/_layout/login": typeof LayoutLoginRoute;
+  "/_layout/mail": typeof LayoutMailRoute;
   "/_layout/profile": typeof LayoutProfileRoute;
   "/_layout/sigunp": typeof LayoutSigunpRoute;
   "/_layout/home/$roomId": typeof LayoutHomeRoomIdRoute;
@@ -176,17 +195,27 @@ export interface FileRouteTypes {
     | "/"
     | ""
     | "/login"
+    | "/mail"
     | "/profile"
     | "/sigunp"
     | "/home/$roomId"
     | "/home";
   fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "" | "/login" | "/profile" | "/sigunp" | "/home/$roomId" | "/home";
+  to:
+    | "/"
+    | ""
+    | "/login"
+    | "/mail"
+    | "/profile"
+    | "/sigunp"
+    | "/home/$roomId"
+    | "/home";
   id:
     | "__root__"
     | "/"
     | "/_layout"
     | "/_layout/login"
+    | "/_layout/mail"
     | "/_layout/profile"
     | "/_layout/sigunp"
     | "/_layout/home/$roomId"
@@ -225,6 +254,7 @@ export const routeTree = rootRoute
       "filePath": "_layout.tsx",
       "children": [
         "/_layout/login",
+        "/_layout/mail",
         "/_layout/profile",
         "/_layout/sigunp",
         "/_layout/home/$roomId",
@@ -233,6 +263,10 @@ export const routeTree = rootRoute
     },
     "/_layout/login": {
       "filePath": "_layout/login.tsx",
+      "parent": "/_layout"
+    },
+    "/_layout/mail": {
+      "filePath": "_layout/mail.tsx",
       "parent": "/_layout"
     },
     "/_layout/profile": {
