448164e3b27ce60625672e4c6ca60de360a1e5bb
ryu sugiyama <2005ryu427@gmail.com>
2025-03-23 11:13:55 +0900

    Update client/src/routes/index.tsx
    Co-authored-by: Imai Raku <71510837+luck-tech@users.noreply.github.com>

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -7,7 +7,9 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-[#FF9350]">
+    <div className={`flex flex-col justify-center items-center min-h-screen bg-[#FF9350] transition-opacity duration-500 ease-in-out ${
+        isVisible ? "opacity-100" : "opacity-0"
+      }`}>
       <SvgSmile className="w-[393px] h-[852px] flex-none order-1 flex-grow-0 " />
       <SvgTitle className="w-[340px] h-[85px] flex-none order-0 mt-20 " />
     </div>
