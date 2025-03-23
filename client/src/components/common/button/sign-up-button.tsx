0b046ab4a5ec917f00ffac1c4abf44abf2604c11
luck-tech <luck@imai.am>
2025-03-23 18:48:22 +0900

    不要な空白、改行の削除

--- client/src/components/common/button/sign-up-button.tsx
+++ client/src/components/common/button/sign-up-button.tsx
@@ -3,8 +3,7 @@ const SignUpButton = () => {
     <div className="flex flex-col justify-center items-center">
       <button
         type="submit"
-        className="relative w-[152px] h-[27px] top-[5px] 
-                text-black text-sm font-medium cursor-pointer  "
+        className="relative w-[152px] h-[27px] top-[5px] text-black text-sm font-medium cursor-pointer"
       >
         新規登録
       </button>
