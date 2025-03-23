c0669f565bd82d0d362e18138f12ff345da55838
luck-tech <luck@imai.am>
2025-03-23 18:48:22 +0900

    不要な空白、改行の削除

--- client/src/components/common/button/sign-up-button.tsx
+++ client/src/components/common/button/sign-up-button.tsx
@@ -8,8 +8,7 @@ const SignUpButton = () => {
         navigate({ to: "/signup" });
       }}
         type="submit"
-        className="relative w-[152px] h-[27px] top-[5px] 
-                text-black text-sm font-medium cursor-pointer  "
+        className="relative w-[152px] h-[27px] top-[5px] text-black text-sm font-medium cursor-pointer"
       >
         新規登録
       </button>
