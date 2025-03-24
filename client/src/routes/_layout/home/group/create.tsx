950e6327a1cd93a6a4fd12ed0cda69f180783218
ユーザー名 <luck@imai.am>
2025-03-25 01:58:31 +0900

    buttonのデザインをedit画面と合わせる, 背景色の変更

--- client/src/routes/_layout/home/group/create.tsx
+++ client/src/routes/_layout/home/group/create.tsx
@@ -8,8 +8,9 @@ export const Route = createFileRoute("/_layout/home/group/create")({
 });
 
 function RouteComponent() {
-  const { handleSubmit, register } = useForm<Group>();
+  const { handleSubmit, register, watch } = useForm<Group>();
   const navigate = useNavigate();
+  const groupNameValue = watch("groupName");
 
   const onSubmit = (data: Group) => {
     //TODO: APIでグループ作成を叩いて、ホーム画面に戻す
@@ -19,16 +20,24 @@ function RouteComponent() {
     }
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="bg-[#FFEADD] h-full">
       <div className="flex flex-col justify-center items-center ">
         <input
           type="text"
           id="text"
           {...register("groupName", { required: "グループ名" })}
           placeholder="グループ名を入力"
-          className="w-[310px] h-[48px] bg-white border border-black rounded px-4 text-black m-[32px_40px_16px_40px] focus:outline-black"
+          className="w-[310px] h-[48px] bg-white border border-black outline-none rounded px-4 text-black m-[32px_40px_16px_40px] focus:outline-black"
         />
-        <button type="submit" className="cursor-pointer">
+        <button
+          type="submit"
+          disabled={!groupNameValue}
+          className={`py-2 px-6 rounded-full shadow-md transition-colors ${
+            !groupNameValue
+              ? "bg-gray-300 cursor-not-allowed"
+              : "bg-[#FF9350] text-white cursor-pointer hover:bg-[#FF7F30]"
+          }`}
+        >
           作成
         </button>
       </div>
