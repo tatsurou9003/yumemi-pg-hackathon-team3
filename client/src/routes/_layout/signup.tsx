84248fb5ff49e2e650d3f7ded7ce21335a1e8e36
ryu sugiyama <2005ryu427@gmail.com>
2025-03-25 15:54:24 +0900

    修正しました

--- client/src/routes/_layout/signup.tsx
+++ client/src/routes/_layout/signup.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import LoginForm from "@/components/common/form/loginform";
-import SignBackButton from "@/components/common/button/login-back-button";
-import SignUpSettingButton from "@/components/common/button/sign-up-setting";
+import LginBackButton from "@/features/signup/login-back-button";
+import SignUpSettingButton from "@/features/signup/sign-up-setting";
 export const Route = createFileRoute("/_layout/signup")({
   component: RouteComponent,
 });
@@ -10,7 +10,7 @@ function RouteComponent() {
     <div className="min-h-screen bg-[#FFBC92] text-xs bg-[url(/src/assets/icons/character.svg)] bg-no-repeat bg-contain bg-center ">
       <LoginForm />
       <SignUpSettingButton />
-      <SignBackButton />
+      <LginBackButton />
     </div>
   );
 }
