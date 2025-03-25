f66a0761e35a7748e68605a4229d57c9fcdad286
renyagi <r.yagi@4nonome.com>
2025-03-26 08:38:52 +0900

    履歴取得機能の追加

--- client/src/types/messageData.ts
+++ client/src/types/messageData.ts
@@ -2,15 +2,15 @@ export type MessageData = {
   messageId: string;
   messageType: string;
   messageText: string;
-  messageImage: string;
-  prizeText: string;
-  deadline: string;
-  winner: string;
+  messageImage?: string;
+  prizeText?: string;
+  deadline?: string;
+  winner?: string;
   createdBy: {
     userId: string;
     userName: string;
-    profileImage: string;
-    profileColor: string;
+    profileImage?: string;
+    profileColor?: string;
   };
   createdAt: string;
 };
