c81903b90d4ae4b816d08ba3b45e8745c01a3a54
ユーザー名 <luck@imai.am>
2025-03-27 16:24:50 +0900

    画像削除

--- client/src/components/common/loading/loading.tsx
+++ client/src/components/common/loading/loading.tsx
@@ -7,16 +7,6 @@ const LoadingIndicator = ({
     <div
       className={`h-full flex flex-col items-center justify-center bg-[#FFBC92]`}
     >
-      <div className="w-16 h-16 relative mb-4 animate-bounce">
-        <img
-          src="/src/assets/icons/character.svg"
-          alt="キャラクター"
-          className="w-full h-full"
-        />
-        <div className="absolute -top-2 -right-2 w-5 h-5 bg-white rounded-full flex items-center justify-center text-[10px] animate-pulse">
-          <span>?</span>
-        </div>
-      </div>
       <div className="text-[#743E3E] font-bold text-lg">{message}</div>
       <div className="mt-2 flex space-x-1">
         {[0, 1, 2].map((dot) => (
